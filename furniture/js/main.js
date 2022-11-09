811303c812c361187d4707f3ca397afbe62b7702
Yuko Okamoto <y.okamoto.0227@gmail.com>
2022-11-09 20:25:31 +0900

    furniture new file

---
+++ furniture/js/main.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.nav-btn').click(function() {
+        $('.nav-bg').toggleClass('is-active');
+        $('.nav').toggleClass('is-active');
+        $('.nav-btn').toggleClass('is-active');
+    });
+});
